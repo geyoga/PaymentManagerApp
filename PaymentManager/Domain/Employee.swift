f9ae14df3d22679dc456feb43287771b1a32ec6e
georgiusyoga <yogadewantama@gmail.com>
2025-02-07 12:39:14 +0700

    Initial Commit

---
+++ PaymentManager/Domain/Employee.swift
@@ -0,0 +1,12 @@
+//
+//  Employee.swift
+//  PaymentManager
+//
+//  Created by Georgius Yoga on 7/2/25.
+//
+
+import Foundation
+
+struct Employee: Equatable {
+    let id = UUID()
+}
---
+++ PaymentManager/PaymentManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentManagerApp.swift
+//  PaymentManager
+//
+//  Created by Georgius Yoga on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PaymentManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
