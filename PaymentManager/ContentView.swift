35b146a63da7989125623bb8b5ca6c34aa3b7430
georgiusyoga <yogadewantama@gmail.com>
2025-02-07 12:39:14 +0700

    Initial Commit

---
+++ PaymentManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaymentManager
+//
+//  Created by Georgius Yoga on 7/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
